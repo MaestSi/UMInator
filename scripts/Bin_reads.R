d88eb80c073b6e65b58feca3448dc5da5f988d41
Simone Maestri <simone.maestri@hotmail.it>
2023-10-06 12:33:09 +0200

    Add files via upload

--- scripts/Bin_reads.R
+++ scripts/Bin_reads.R
@@ -39,15 +39,26 @@ Bin_reads <- function(fastq_file, alignments_file, outdir) {
   #assign reads to UMIs
   data <- data.frame(reads, UMIs)
   data_split <- split(data$reads, data$UMIs)
+  maxLen <- 20000
   #cycle across UMIs and split reads to files
   for (u in 1:length(data_split)) {
     fastq_reads_curr_UMI <- fastq_reads[which(names(fastq_reads) %in% data_split[[u]])]
+    tbd <- names(fastq_reads_curr_UMI[which(width(fastq_reads_curr_UMI) >= maxLen)])
+    if (length(tbd) > 0) {
+      cat(sprintf("Discarding the following reads, because they are longer than %d bp: %s\n", maxLen, paste0(tbd, collapse = ", ")))
+      fastq_reads_curr_UMI <- fastq_reads_curr_UMI[which(width(fastq_reads_curr_UMI) < maxLen)]
+    }
     writeXStringSet(x = fastq_reads_curr_UMI, filepath = paste0(outdir, "/", names(data_split[u]), "_chunk_", basename(fastq_file)), append = TRUE, format = "fastq")
   }
   #write unbinned reads to file
   unbinned_reads_ids <- setdiff(names(fastq_reads), reads)
   if (length(unbinned_reads_ids) > 0) {
     unbinned_reads <- fastq_reads[which(names(fastq_reads) %in% unbinned_reads_ids)]
+    tbd <- names(unbinned_reads[which(width(unbinned_reads) >= maxLen)])
+    if (length(tbd) > 0) {
+      cat(sprintf("Discarding the following reads, because they are longer than %d bp: %s\n", maxLen, paste0(tbd, collapse = ", ")))
+      unbinned_reads <- unbinned_reads[which(width(unbinned_reads) < maxLen)]
+    }
     writeXStringSet(x = unbinned_reads, filepath = paste0(outdir, "/unbinned_chunk_", basename(fastq_file)), append = TRUE, format = "fastq")
   }
 }
